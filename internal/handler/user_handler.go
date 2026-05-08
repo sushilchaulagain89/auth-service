3d895b263221bfa9f1be3f01a93ef691762f68fe
Sushil Chaulagain <sushil.hancy123@gmail.com>
2026-05-08 10:20:29 +0545

    user handler for user creation

---
+++ internal/handler/user_handler.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"net/http"
+
+	"auth-service/internal/repository"
+	"auth-service/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+
+	var req struct {
+		Email string `json:"email"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
+	}
+
+	repo := &repository.UserRepository{}
+	svc := &service.UserService{Repo: repo}
+
+	err := svc.CreateUser(req.Email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "db error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "user created"})
+}
