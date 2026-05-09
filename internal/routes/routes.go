0a66ebcf6f1715937bd86d8d4527b8298e1ca07c
Sushil Chaulagain <sushil.hancy123@gmail.com>
2026-05-09 15:09:08 +0545

    updated: user route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -6,7 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRoutes(router *gin.Engine){
-	router.GET("/health",handler.HealthCheck)
-	router.POST("/user", handler.CreateUser)
+func RegisterRoutes(router *gin.Engine,userHandler *handler.UserHandler){
+	router.POST("/user",userHandler.CreateUser)
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
 } 
