b6ffe94d3d1cdc77975762ec9a3437866bdc2cd5
Sushil Chaulagain <sushil.hancy123@gmail.com>
2026-05-09 17:46:10 +0545

    implemented get user by email

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -4,10 +4,26 @@ import (
 	"auth-service/internal/db"
 	"context"
 )
- type UserRepository struct{}
 
- func (r *UserRepository) CreateUser(email string,passwordHash string) error {
+type UserRepository struct{}
+type User struct {
+	Email        string
+	PasswordHash string
+}
+
+func (r *UserRepository) CreateUser(email string, passwordHash string) error {
 	query := "INSERT INTO users (email,password_hash) VALUES ($1,$2)"
-	_,err := db.Pool.Exec(context.Background(),query,email,passwordHash)
-	return  err
- }
+	_, err := db.Pool.Exec(context.Background(), query, email, passwordHash)
+	return err
+}
+
+func (r *UserRepository) GetUserByEmail(email string) (*User, error) {
+	query := "SELECT email,password_hash FROM users where email=$1"
+	row := db.Pool.QueryRow(context.Background(), query, email)
+	user := &User{}
+	err := row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil,err
+	}
+	return user, err
+}
