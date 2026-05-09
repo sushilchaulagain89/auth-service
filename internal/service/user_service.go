5a372d85022d4e3553232c79fcf67c52362c5fad
Sushil Chaulagain <sushil.hancy123@gmail.com>
2026-05-09 16:43:40 +0545

    updated: password hash to prevent raw password save

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,12 +1,20 @@
 package service
+
 import (
 	"auth-service/internal/repository"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
 	Repo *repository.UserRepository
 }
 
-func (s *UserService) CreateUser(email string) error{
-	return  s.Repo.CreateUser(email)
-} 
+func (s *UserService) CreateUser(email string ,password string) error{
+	passwordBytes := []byte(password)
+	passwordHash,err := bcrypt.GenerateFromPassword(passwordBytes,bcrypt.DefaultCost)
+	if err != nil{
+		return err
+	}
+	return  s.Repo.CreateUser(email,string(passwordHash))
+}
