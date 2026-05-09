136cd7abcb244e9f59cd86ad6236d7e06f69925a
Sushil Chaulagain <sushil.hancy123@gmail.com>
2026-05-09 17:46:55 +0545

    implemented error haldle and password compare

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"auth-service/internal/repository"
+	"errors"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -10,11 +11,34 @@ type UserService struct {
 	Repo *repository.UserRepository
 }
 
-func (s *UserService) CreateUser(email string ,password string) error{
+var (
+	ErrEmailExists  = errors.New("email already exists")
+	ErrInvalidLogin = errors.New("invalid credentials")
+	ErrUserNotFound = errors.New("user not found")
+)
+
+func (s *UserService) CreateUser(email string, password string) error {
 	passwordBytes := []byte(password)
-	passwordHash,err := bcrypt.GenerateFromPassword(passwordBytes,bcrypt.DefaultCost)
-	if err != nil{
+	passwordHash, err := bcrypt.GenerateFromPassword(passwordBytes, bcrypt.DefaultCost)
+	if err != nil {
 		return err
 	}
-	return  s.Repo.CreateUser(email,string(passwordHash))
+	return s.Repo.CreateUser(email, string(passwordHash))
+}
+
+func (s *UserService) Login(email, password string) error {
+	user, err := s.Repo.GetUserByEmail(email)
+	if err != nil {
+		return ErrInvalidLogin
+	}
+
+	err = bcrypt.CompareHashAndPassword(
+		[]byte(user.PasswordHash),
+		[]byte(password),
+	)
+	if err != nil {
+		return ErrInvalidLogin
+	}
+
+	return nil
 }
