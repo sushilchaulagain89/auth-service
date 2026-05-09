5582fb5597c35ff7af0608cfd1ca9887b6192edc
Sushil Chaulagain <sushil.hancy123@gmail.com>
2026-05-09 15:08:38 +0545

    user service handler

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -3,27 +3,27 @@ package handler
 import (
 	"net/http"
 
-	"auth-service/internal/repository"
 	"auth-service/internal/service"
 
 	"github.com/gin-gonic/gin"
 )
 
-func CreateUser(c *gin.Context) {
+type UserHandler struct {
+	Service *service.UserService
+}
+
+func (h *UserHandler) CreateUser(c *gin.Context) {
 
 	var req struct {
 		Email string `json:"email"`
 	}
 
 	if err := c.ShouldBindJSON(&req); err != nil || req.Email == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email is required"})
 		return
 	}
 
-	repo := &repository.UserRepository{}
-	svc := &service.UserService{Repo: repo}
-
-	err := svc.CreateUser(req.Email)
+	err := h.Service.CreateUser(req.Email)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
 		return
